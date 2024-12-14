5e3d08ee7571cbf32527fd7af787fa6b6e5cc9fd
Max <maxmarsyyz@icloud.com>
2024-12-13 22:19:45 -0500

    Added controls to make player accelerate/slow down

--- main.lua
+++ main.lua
@@ -6,38 +6,45 @@ function love.load()
     player.y = 200
     player.radius = 25
     player.speed = 3
-    player.turningspeed = 2
+    player.topSpeed = 12
+    player.turningspeed = 3
     player.dir = 0
+    player.acceleration = 5
+    player.braking = 12
+    player.resistance = 2.5
 end
 
 look = 0
 function love.update(dt)
-    
     look = player:lookat(love.mouse.getX(), love.mouse.getY(), dt)
-    player:move()
+    player:move(dt)
+
 end
 
 function love.draw()
-    love.graphics.setColor(0,1,0, 1)
-    love.graphics.circle('fill', player.x, player.y, player.radius) -- player 
-    local nosePosition = vectorfromangle(player.dir, player.radius)
-    love.graphics.setColor(1,0,0, 1)
-    love.graphics.circle('fill', nosePosition[1] + player.x, nosePosition[2] + player.y, 5) --player nose
-    love.graphics.print(gameobject, 300, 300)
-end
-
-function follow() -- puts the player right on the mouse
-    local x,y = love.mouse.getX(), love.mouse.getY()
-    local diff = {['x'] = x - player.x, ['y'] = y - player.y}
-    normalize(diff)
-    player.x = player.x + diff.x * player.speed
-    player.y = player.y + diff.y * player.speed
+    player:draw()
 end
 
 function normalize(vec) -- normalize a 2d vector
     local mag = math.sqrt(vec.x^2 + vec.y ^ 2)
     vec.x = vec.x / mag
     vec.y = vec.y / mag
+end
+
+function player.draw(self)
+    love.graphics.setColor(0,1,0, 1)
+    love.graphics.circle('fill', player.x, player.y, player.radius)
+
+    local nosePosition = vectorfromangle(player.dir, player.radius)
+    love.graphics.setColor(1,0,0, 1)
+    love.graphics.circle('fill', nosePosition[1] + player.x, nosePosition[2] + player.y, 5)
+
+    love.graphics.setColor(0,0,1, 1)
+    love.graphics.circle('fill', player.x, player.y, 15)
+end
+
+function player.acceleration(self, dt)
+    
 end
 
 function player.lookAngle(self, x, y) -- finds the angle between the player and a point
@@ -89,13 +96,36 @@ function player.lookat(self, x, y, delta, instant) -- rotates the player towards
     return angle
 end
 
-function player.move(self)
+function player.move(self, dt) -- calculate acceleration and move the player
+
+
+    -- first calcuate player acceleration/braking
+    if love.keyboard.isDown('w') then
+        player.speed = player.speed + player.acceleration * dt
+    elseif love.keyboard.isDown('s') then
+        player.speed = player.speed - player.braking * dt
+    end
+
+    player.speed = player.speed - player.resistance * dt -- wind/friction resistance
+
+    if player.speed < 0 then -- contrain speed to 0 and top speed
+        player.speed = 0
+    elseif player.speed > player.topSpeed then
+        player.speed = player.topSpeed
+    end
+
+    -- then calculate movement
+    local x = love.mouse.getX() - player.x
+    local y = love.mouse.getY() - player.y
+    -- if x * x + y * y < 225 then -- player stops when at mouse position
+    --     return
+    -- end
     vel = vectorfromangle(self.dir, self.speed)
     player.x = vel[1] + player.x
     player.y = vel[2] + player.y
 end
 
-function vectorfromangle(rads, mag)
+function vectorfromangle(rads, mag) -- creates a vector when given an angle and a length/magnitude
     if (mag == nil) then mag = 1 end
     return {math.sin(rads) * mag, math.cos(rads) * mag}
 end
