5e624204d112e24008beaebb190c04ac2239db7e
Max <maxmarsyyz@icloud.com>
2025-01-20 14:22:57 -0500

    created enemy detection function, untested

--- FactoryDefense/defensebot.lua
+++ FactoryDefense/defensebot.lua
@@ -31,12 +31,24 @@ function defensebot:new()
     if(o.yvalue < 350) then
         o.ydir = -1
     end
-
+    --stats
+    o.speed = 5
+    o.health = 100
+    o.range = 15
+    o.rangeSq = o.range * o.range
+    o.damage = 25
+    o.attackspeed = 1
     return o
 end
 
 
 function defensebot:update(dt)
+    if(not self.attack()) then
+        self.move()
+    end
+end
+
+function move()
     if(abs(self.y - self.yvalue) > 3) then
         self.y = self.y + self.speed*dt*self.ydir
     end
@@ -44,6 +56,32 @@ function defensebot:update(dt)
     if(self.x > 400) then
         self.delete = true
     end
+end
+
+
+--[[
+    checks for targets in range and sets target to closest
+    returns true if enemy found, otherwise false
+    ** untested! **
+]]
+function defensebot:attack()
+    target = nil
+    prev = 999
+    for i in ipairs(enemies) do
+        x = enemies[i].x - self.x
+        y = enemies[i].y - self.y
+        dist = x*x + y*y
+        if dist < self.rangeSq then
+            if(dist < prev) then
+                target = enemies[i]
+                prev = dist
+            end
+        end
+    end
+    if (target) then return true 
+    else return false end
+    -- check for enemy in range
+    -- fire at enemy
 end
 
 function defensebot:draw()
